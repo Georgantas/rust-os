01948fe5829c9d270d7b6e38428878b0d30d8a85
Georgantas <18753033+Georgantas@users.noreply.github.com>
2022-05-30 11:21:41 -0400

    Checkpoint.

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(rust_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use rust_os::println;
+
+// No mangle ensure that the Rust compiler really
+// outputs a function with the name _start.
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello world!");
+
+    rust_os::init();
+
+    fn stack_overflow() {
+        stack_overflow();
+    }
+
+    stack_overflow();
+
+    #[cfg(test)]
+    test_main();
+
+    println!("No crash!");
+
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rust_os::test_panic_handler(info)
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1 + 1, 2);
+}
---
+++ tests/stack_overflow.rs
@@ -0,0 +1,33 @@
+
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+use rust_os::serial_print;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    serial_print!("stack_overflow::stack_overflow...\t");
+
+    rust_os::gdt::init();
+    
+    init_test_idt();
+
+    stack_overflow();
+
+    panic!("Execution continued after stack overflow.")
+}
+
+#[allow(unconditional_recursion)]
+fn stack_overflow() {
+    stack_overflow();
+    // prevent tail recursion optimization
+    volatile::Volatile::new(0).read();
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rust_os::test_panic_handler(info)
+}
+
+
