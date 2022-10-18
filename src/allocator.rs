124fe2836adc4e40a5bd007d1720aeba36722eb0
Georgantas <18753033+Georgantas@users.noreply.github.com>
2022-10-17 20:58:51 -0400

    Complete Allocator Designs.

--- src/allocator.rs
+++ src/allocator.rs
@@ -6,8 +6,10 @@ use linked_list_allocator::LockedHeap;
 use x86_64::structures::paging::mapper::MapToError;
 use x86_64::structures::paging::{FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB};
 use x86_64::VirtAddr;
+use crate::allocator::fixed_size_block::FixedSizeBlockAllocator;
 
 pub mod bump;
+pub mod fixed_size_block;
 pub mod linked_list;
 
 /// Align the given address `addr` upwards to alignment `align`.
@@ -43,7 +45,7 @@ impl<A> Locked<A> {
 }
 
 #[global_allocator]
-static ALLOCATOR: Locked<LinkedListAllocator> = Locked::new(LinkedListAllocator::new());
+static ALLOCATOR: Locked<FixedSizeBlockAllocator> = Locked::new(FixedSizeBlockAllocator::new());
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
